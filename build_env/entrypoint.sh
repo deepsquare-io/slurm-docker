8567507de7284da4add8c690cffca913ef50e9c7
root <root>
2021-06-22 12:49:28 +0000

    entrypoint.sh

--- build_env/entrypoint.sh
+++ build_env/entrypoint.sh
@@ -1,16 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
 # Munge
 cp /secrets/munge.key /etc/munge/munge.key && chown munge: /etc/munge/munge.key
 su -s /bin/bash -c munged munge
 
 # SSSD
-rm -rf /var/run/sssd.pid
+rm -f /var/run/sssd.pid
 /etc/init.d/sssd restart
 
+while ! id slurm
+do
+  sleep 1
+done
+
 # Slurm
-mkdir /var/run/slurm && cp /secrets/jwt_hs256.key /var/spool/slurm/ctld/
-chown -R slurm: /var/spool/slurm /var/run/slurm
+mkdir -p /var/spool/slurmctl
+mkdir -p /var/run/slurmctl
+mkdir -p /var/log/slurm
+mkdir -p /var/spool/slurm
+mkdir -p /var/run/slurm && cp /secrets/jwt_hs256.key /var/spool/slurmctl/
+chown -R slurm: /var/spool/slurm /var/run/slurm /var/run/slurmctl /var/spool/slurmctl
 slurmctld -D
 
 ### Debug: start slurmctld in background and tail log instead - container remains up even if slurmctld crashes
