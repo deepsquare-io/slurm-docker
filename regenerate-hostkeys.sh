1a9a681189c5960ec7189c522f0ad7936fc21841
Nguyen Marc <nguyen_marc@live.fr>
2022-02-09 14:17:09 +0100

    feat: generate keys at the right place
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- regenerate-hostkeys.sh
+++ regenerate-hostkeys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir -p ./secrets/hostkeys
-yes 'y' | ssh-keygen -N '' -f ./secrets/hostkeys/ssh_host_rsa_key -t rsa -C headnode
-yes 'y' | ssh-keygen -N '' -f ./secrets/hostkeys/ssh_host_ecdsa_key -t ecdsa -C headnode
-yes 'y' | ssh-keygen -N '' -f ./secrets/hostkeys/ssh_host_ed25519_key -t ed25519 -C headnode
+mkdir -p ./secrets/ssh
+yes 'y' | ssh-keygen -N '' -f ./secrets/ssh/ssh_host_rsa_key -t rsa -C headnode
+yes 'y' | ssh-keygen -N '' -f ./secrets/ssh/ssh_host_ecdsa_key -t ecdsa -C headnode
+yes 'y' | ssh-keygen -N '' -f ./secrets/ssh/ssh_host_ed25519_key -t ed25519 -C headnode
 chmod 600 ./hostkeys/ssh_host_*_key
