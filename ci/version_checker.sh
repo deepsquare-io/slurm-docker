11088c8a093b8033d27705e7b29386bfe7313639
Nguyen Marc <nguyen_marc@live.fr>
2022-05-31 13:41:31 +0200

    feat(ci): added version check
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

---
+++ ci/version_checker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+
+slurm_version=$(curl --silent https://api.github.com/repos/schedmd/slurm/tags | jq '.[1].name' | awk '{ print substr( $0, 2, length($0)-2 ) }')
+slurm_tag=$(curl --silent https://api.github.com/repos/schedmd/slurm/tags | jq -r '.[1].name')
+META="$(curl -fsSL "https://raw.githubusercontent.com/SchedMD/slurm/${slurm_tag}/META")"
+slurm_version=$(echo "${META}" | grep 'Version:' | sed -E 's/.*Version:\t+(.*)/\1/g')
+slurm_release=$(echo "${META}" | grep 'Release:' | sed -E 's/.*Release:\t+(.*)/\1/g')
+slurm_definitive_version="${slurm_version}-${slurm_release}"
+sed -i "s/slurm_version=.*\$/slurm_version=${slurm_definitive_version}/g" ./slurm/Dockerfile
+sed -i "s/slurm_version=.*\$/slurm_version=${slurm_definitive_version}/g" ./slurm/build-all.sh
--- slurm/build-all.sh
+++ slurm/build-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker build . -t ghcr.io/squarefactory/slurm:21.08.8-2-controller --target slurm-controller
-docker build . -t ghcr.io/squarefactory/slurm:21.08.8-2-login --target slurm-login
-docker build . -t ghcr.io/squarefactory/slurm:21.08.8-2-rest --target slurm-rest
-docker build . -t ghcr.io/squarefactory/slurm:21.08.8-2-db --target slurm-db
-docker build . -t ghcr.io/squarefactory/slurm:21.08.8-2-compute --target slurm-compute
+slurm_version=22.05.0-1
+docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-controller --target slurm-controller
+docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-login --target slurm-login
+docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-rest --target slurm-rest
+docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-db --target slurm-db
+docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-compute --target slurm-compute
