035431ca7b87b28707d706a79dbca2296029a772
Nguyen Marc <nguyen_marc@live.fr>
2022-08-12 15:19:01 +0200

    feat: rocky9
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

---
+++ slurm/build-all.rocky8.5.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+slurm_version=22.05.3-1
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-controller --target slurm-controller
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-login --target slurm-login
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-rest --target slurm-rest
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-db --target slurm-db
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-compute --target slurm-compute
+docker build . -f Dockerfile.rocky8.5 -t ghcr.io/squarefactory/slurm:${slurm_version}-prometheus-exporter --target slurm-prometheus-exporter
--- slurm/build-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-slurm_version=22.05.2-1
-docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-controller --target slurm-controller
-docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-login --target slurm-login
-docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-rest --target slurm-rest
-docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-db --target slurm-db
-docker build . -t ghcr.io/squarefactory/slurm:${slurm_version}-compute --target slurm-compute
