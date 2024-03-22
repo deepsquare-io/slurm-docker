7071ef14a36caf99c64b739381d099b8a7609a8d
Darkness4 <18192844+Darkness4@users.noreply.github.com>
2024-03-22 00:00:45 +0000

    chore: update dependencies

--- slurm/build-all.rocky9.2.sh
+++ slurm/build-all.rocky9.2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-slurm_version=23.02.6-1
+slurm_version=23.11.5-1
 docker build . -f Dockerfile.rocky9.2 -t ghcr.io/squarefactory/slurm:${slurm_version}-controller --target slurm-controller
 docker build . -f Dockerfile.rocky9.2 -t ghcr.io/squarefactory/slurm:${slurm_version}-login --target slurm-login
 docker build . -f Dockerfile.rocky9.2 -t ghcr.io/squarefactory/slurm:${slurm_version}-rest --target slurm-rest
